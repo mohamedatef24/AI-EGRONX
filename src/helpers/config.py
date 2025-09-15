d20e20921827aafccef1239c5a4d9cc52beb1a77
mohamed atef <mo7amed3atf24@gmail.com>
2025-09-15 18:31:09 +0300

    file upload

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,8 +4,9 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
-    FILE_ALLOWED_EXTENSIONS: list
-    FILE_MAX_SIZE_MB: int
+    FILE_ALLOWED_TYPES: list
+    FILE_MAX_SIZE: int
+    FILE_DEFAULT_CHUNK_SIZE: int
     class Config:
         env_file = ".env"
         
