551aa0da9bc1e048c8c2ca9996a5f1cbc8f8dce3
mohamed atef <mo7amed3atf24@gmail.com>
2025-09-17 18:50:45 +0300

    added cohere

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -7,6 +7,20 @@ class Settings(BaseSettings):
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
     FILE_DEFAULT_CHUNK_SIZE: int
+    
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
     class Config:
         env_file = ".env"
         
--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from routes import base,data
+from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 
 app = FastAPI()
+
+settings = get_settings()
+llm_provider_factory = LLMProviderFactory(settings)
+
+app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+app.generation_client.set_generation_model(settings.GENERATION_MODEL_ID)
+
+app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
 
